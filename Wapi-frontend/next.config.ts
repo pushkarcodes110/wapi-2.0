fdb30d54669b224d69cf11f408facbdba60db7ba
Pushkar <pushkarrawasthi@gmail.com>
2026-05-19 20:33:41 +0530

    feat: add Docker configuration and standalone build support with root redirect to landing page

--- Wapi-frontend/next.config.ts
+++ Wapi-frontend/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
+  output: "standalone",
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
